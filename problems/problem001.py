7ef8bd2ac5ee290da7030b8e792fc0a4f5b13541
cvturner <cvturner@usc.edu>
2019-07-31 19:10:36 -0700

    Create problem001.py in problems folder

---
+++ problems/problem001.py
@@ -0,0 +1,16 @@
+'''
+Catherine Turner
+7/31/19
+https://projecteuler.net// Problem 1
+'''
+
+def multipleSum(a, b, max):
+    current = 1
+    sum = 0
+    while current < max:
+        if ((current % a == 0) or (current % b == 0)):
+            sum += current
+        current += 1
+    return sum
+
+print(multipleSum(3, 5, 1000))
