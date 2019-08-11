19b4940ce5cfc77965ae35c7b421db1836ab9b16
cvturner <cvturner@usc.edu>
2019-08-10 20:15:25 -0700

    Corrected for max values less than 1000

--- problems/problem017.py
+++ problems/problem017.py
@@ -13,13 +13,13 @@ def countNums(max):
     letterSum = 0
 
     # Compute letter count for numbers 1-19
-    while current < 20 and current != max:
+    while current < 20 and current != (max+1):
         for letter in uniqueNums[current-1]:
             letterSum += 1
         current += 1
 
     # Compute letter count for numbers 20-99
-    while current > 19 and current < 100 and current != max:
+    while current > 19 and current < 100 and current != (max+1):
         currentString = str(current)
         for letter in twoDigitNums[int(currentString[0])-2]:
             letterSum += 1
@@ -29,7 +29,7 @@ def countNums(max):
         current += 1
 
     # Compute letter count for numbers 100-999
-    while current > 99 and current < 1000 and current != max:
+    while current > 99 and current < 1000 and current != (max+1):
         currentString = str(current)
         for letter in uniqueNums[int(currentString[0])-1] + "hundred":
             letterSum += 1
@@ -47,7 +47,7 @@ def countNums(max):
         current += 1
 
     # Compute letter count for 1000 (max for this program)
-    if current == 1000:
+    if max == 1000:
         for letter in "one" + "thousand":
             letterSum += 1
 
